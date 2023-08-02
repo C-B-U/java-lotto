35a506d45633724021de7c98dd6a3d67fd1059f6
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-03 01:54:04 +0900

    feat(InputValidator): 로또 구매 금액 유효성 검사

--- src/main/java/lotto/InputValidator.java
+++ src/main/java/lotto/InputValidator.java
@@ -6,7 +6,11 @@ public class InputValidator {
     private static final int LOTTO_PRICE = 1000;
     private static final int IS_THOUSAND_WON_UNIT = 0;
 
-    public void validatePurchaseAmount(String purchaseAmount) {
+
+    public static void validatePurchaseAmount(String purchaseAmount) {
+        if(Integer.parseInt(purchaseAmount)%LOTTO_PRICE != IS_THOUSAND_WON_UNIT) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 1000원 단위여야 합니다.");
+        }
     }
 
     public void validateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
