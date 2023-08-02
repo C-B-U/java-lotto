3ecc968e667025b9e88c2bd622e8ac150695b55e
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-03 01:28:57 +0900

    feat(InputValidator): 입력 값 예외처리 틀

---
+++ src/main/java/lotto/InputValidator.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import java.util.List;
+
+public class InputValidator {
+    private static final int LOTTO_PRICE = 1000;
+    private static final int IS_THOUSAND_WON_UNIT = 0;
+
+    public void validatePurchaseAmount(String purchaseAmount) {
+    }
+
+    public void validateBonusNumber(List<Integer> winningNumbers, int bonusNumber) {
+        // 보너스 번호 검사
+    }
+
+    public void validateWinningNumber() {
+        // 당첨 번호 검사
+    }
+}
