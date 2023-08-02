5e8a11d3afc1bbb407702da48cfb28f59a3e38fe
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-03 01:27:46 +0900

    feat(GeneratorValidator): 로또 번호 생성 예외처리 틀

---
+++ src/main/java/lotto/GeneratorValidator.java
@@ -0,0 +1,17 @@
+package lotto;
+
+public class GeneratorValidator {
+    public void validateDuplicateLottoNumber() {
+        // 로또 중복 확인
+    }
+
+    public void validateSizeLottoNumber() {
+        // 로또 크기 확인 (총 6개)
+
+    }
+
+    public void validateRangeLottoNumber() {
+        // 로또 범위 확인 (1 ~ 45)
+
+    }
+}
