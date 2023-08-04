c557aac22f74987a0ed1460f841c18b914e46477
GaBaljaintheroom <junsu1222@naver.com>
2023-08-04 21:29:29 +0900

    test: 로또 구입 금액 입력

---
+++ src/test/java/lotto/InputValidTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import lotto.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputValidTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    @DisplayName("로또 구입 금액이 1,000원 단위가 아니면 예외가 발생한다.")
+    void inputLottoAmountIsNotThousand(){
+        assertThatThrownBy(() -> memberService.convertLottoAmount("14500"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
