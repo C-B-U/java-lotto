5983e416d8d1eeeccdb4fc3ab0c1a54a6ce37369
GaBaljaintheroom <junsu1222@naver.com>
2023-08-04 21:08:17 +0900

    refactor: 로또 구입 금액 입력 예외 처리 적용

--- src/main/java/lotto/member/service/MemberService.java
+++ src/main/java/lotto/member/service/MemberService.java
@@ -1,17 +1,22 @@
 package lotto.member.service;
 
+import lotto.member.exception.InputValidator;
 import lotto.member.repository.MemberRepository;
 
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final InputValidator inputValidator;
 
     public MemberService(){
         this.memberRepository = new MemberRepository();
+        this.inputValidator = new InputValidator();
     }
 
     public int convertLottoAmount(String lottoAmount){
-        return Integer.parseInt(lottoAmount);
+        int lottoAmountInt = Integer.parseInt(lottoAmount);
+        inputValidator.validateInputLottoAmount(lottoAmountInt);
+        return lottoAmountInt;
     }
 
 
