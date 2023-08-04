082f9408dd91ccf4e7af405f0fd17a839b4138b9
GaBaljaintheroom <junsu1222@naver.com>
2023-08-05 00:18:10 +0900

    feat: 로또 생성 Repository 작성

---
+++ src/main/java/lotto/producer/repository/ProducerRepository.java
@@ -0,0 +1,17 @@
+package lotto.producer.repository;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProducerRepository {
+
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public List<Integer> createLottoNumber(){
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        lottos.add(new Lotto(lottoNumbers));
+        return lottoNumbers;
+    }
+
+}
