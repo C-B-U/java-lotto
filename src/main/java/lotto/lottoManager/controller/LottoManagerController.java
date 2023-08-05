7eaa5a0c86637d96a26a920aacfd58b8ba574d89
GaBaljaintheroom <junsu1222@naver.com>
2023-08-05 17:56:04 +0900

    feat: 로또 결과 통계 Controller 작성

---
+++ src/main/java/lotto/lottoManager/controller/LottoManagerController.java
@@ -0,0 +1,29 @@
+package lotto.lottoManager.controller;
+
+import lotto.lottoManager.service.LottoManagerService;
+import lotto.member.controller.MemberController;
+import lotto.producer.controller.ProducerController;
+import lotto.producer.repository.Lotto;
+
+import java.util.List;
+
+public class LottoManagerController {
+
+    private final MemberController memberController;
+    private final ProducerController producerController;
+    private final LottoManagerService lottoManagerService;
+
+    LottoManagerController(){
+        this.memberController = new MemberController();
+        this.producerController = new ProducerController();
+        this.lottoManagerService = new LottoManagerService();
+    }
+
+    public void startLotto(){
+       int lottoAmount =  memberController.inputLottoAmount();
+       List<Lotto> lottos = producerController.getLottoNumbers(lottoAmount);
+       List<Integer> winNumber = memberController.inputWinLottoNumber();
+       int bonusNumber = memberController.inputBonusNumber();
+       lottoManagerService.getStatistics(lottos, winNumber,bonusNumber);
+    }
+}
