43bae2675971f03c7014ef94378edc9684bffaa1
GaBaljaintheroom <junsu1222@naver.com>
2023-08-05 12:56:40 +0900

    test: 로또 당첨번호 타입 변경 후 저장되어 반환 확인

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lotto.member.repository.MemberRepository;
+import lotto.member.service.MemberService;
 import lotto.producer.repository.Lotto;
 import lotto.producer.repository.ProducerRepository;
 import lotto.producer.service.ProducerService;
@@ -45,6 +47,24 @@ class LottoTest {
 
         //then
         assertThat(resultLottoNumber).isEqualTo("[1, 2, 3, 4, 5, 6]");
+
+    }
+
+    @DisplayName("로또 당첨번호가 타입이 변경된 후 저장소에 저장되어 반환하는지 확인한다.")
+    @Test
+    void saveWinLottoNumberInRepository(){
+        //given
+        MemberService memberService = new MemberService();
+        MemberRepository memberRepository = new MemberRepository();
+        List<Integer> winLottoNumber = memberService.getWinLottoNumber("1,2,3,4,5,6");
+
+        //when
+        for (Integer num : winLottoNumber){
+            memberRepository.save(num);
+        }
+
+        //then
+        assertThat(winLottoNumber).isEqualTo(memberRepository.getWinLottoNumber());
 
     }
 }
