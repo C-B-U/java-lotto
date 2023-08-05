b7e2183a96356935e4f9b914c5bb144a3f31dd4a
GaBaljaintheroom <junsu1222@naver.com>
2023-08-06 01:31:29 +0900

    refactor: 로또 앱 실행 에러처리 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,12 @@ import lotto.lottoManager.controller.LottoManagerController;
 
 public class Application {
     public static void main(String[] args) {
-        LottoManagerController lottoManagerController = new LottoManagerController();
-        lottoManagerController.startLotto();
+        try{
+            LottoManagerController lottoManagerController = new LottoManagerController();
+            lottoManagerController.startLotto();
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
