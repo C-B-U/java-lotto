ac2fe415b10bad2ad5891701e94bbb8cf49e088c
GaBaljaintheroom <junsu1222@naver.com>
2023-08-05 12:41:21 +0900

    feat: 로또 당첨번호 저장 Repository 작성

--- src/main/java/lotto/member/repository/MemberRepository.java
+++ src/main/java/lotto/member/repository/MemberRepository.java
@@ -1,4 +1,17 @@
 package lotto.member.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MemberRepository {
+
+    private final List<Integer> winLottoNumber = new ArrayList<>();
+
+    public void save(Integer number){
+        winLottoNumber.add(number);
+    }
+
+    public List<Integer> getWinLottoNumber() {
+        return winLottoNumber;
+    }
 }
