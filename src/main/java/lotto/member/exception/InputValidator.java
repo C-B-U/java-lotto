a5c288c946ca70c7c338dca6e6ea5735e72ca533
GaBaljaintheroom <junsu1222@naver.com>
2023-08-05 12:40:03 +0900

    style: 로또 번호 validate InputValidator로 이동

--- src/main/java/lotto/member/exception/InputValidator.java
+++ src/main/java/lotto/member/exception/InputValidator.java
@@ -1,10 +1,21 @@
 package lotto.member.exception;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class InputValidator {
 
     public void validateInputLottoAmount(Integer lottoAMount){
         if(lottoAMount % 1000 != 0){
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1,000 단위 입니다.");
+        }
+    }
+
+    public void validateInputLottoNumber(List<Integer> numbers) {
+        Set<Integer> numberCount = new HashSet<>(numbers);
+        if (numberCount.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않는 6자리 수 입니다.");
         }
     }
 }
