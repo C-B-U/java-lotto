ac3cdfe0b346d0b788dceba20897721f0083d677
GaBaljaintheroom <junsu1222@naver.com>
2023-08-06 21:29:23 +0900

    refactor: enum 키 반복문 형태로 수정

--- src/main/java/lotto/lottoManager/repository/LottoManagerRepository.java
+++ src/main/java/lotto/lottoManager/repository/LottoManagerRepository.java
@@ -9,22 +9,10 @@ import java.util.Map;
 public class LottoManagerRepository {
     private final Map<LottoStatistics, Integer> statisticsCounter = new EnumMap<>(LottoStatistics.class);
 
-    public void upMatchCount(List<Integer> lottoNumber, List<Integer> winNumber, int bonusNumber) {
+    public void upStatisticsMatchCount(List<Integer> lottoNumber, List<Integer> winNumber, int bonusNumber) {
         int matchCount = getMatchCount(lottoNumber, winNumber);
-        boolean isContainBonusNumber = isContainBonusNumber(winNumber, bonusNumber);
-        resultMathThird(matchCount);
-        resultMathFourth(matchCount);
-        resultMathFifth(matchCount);
-        resultMathFifthAndBonus(matchCount, isContainBonusNumber);
-        resultMatchSixth(matchCount);
-    }
-
-    public String getMatchResult() {
-        return String.valueOf(LottoStatistics.THIRD) + statisticsCounter.getOrDefault(LottoStatistics.THIRD, 0) + "개\n" +
-                LottoStatistics.FOURTH + statisticsCounter.getOrDefault(LottoStatistics.FOURTH, 0) + "개\n" +
-                LottoStatistics.FIFTH + statisticsCounter.getOrDefault(LottoStatistics.FIFTH, 0) + "개\n" +
-                LottoStatistics.FIFTH_AND_BONUS + statisticsCounter.getOrDefault(LottoStatistics.FIFTH_AND_BONUS, 0) + "개\n" +
-                LottoStatistics.SIXTH + statisticsCounter.getOrDefault(LottoStatistics.SIXTH, 0) + "개\n";
+        boolean isMatchBonusNumber = isMatchBonusNumber(winNumber, bonusNumber);
+        upMatchCount(matchCount, isMatchBonusNumber);
     }
 
     private int getMatchCount(List<Integer> lottoNumbers, List<Integer> winNumber) {
@@ -33,38 +21,16 @@ public class LottoManagerRepository {
                 .count();
     }
 
-    private boolean isContainBonusNumber(List<Integer> winNumber, int bonusNumber){
+    private boolean isMatchBonusNumber(List<Integer> winNumber, int bonusNumber){
         return winNumber.contains(bonusNumber);
     }
 
-
-    private void resultMathThird(int matchCount) {
-        if (matchCount == LottoStatistics.THIRD.getMatchCount()){
-            statisticsCounter.put(LottoStatistics.THIRD, statisticsCounter.getOrDefault(LottoStatistics.THIRD, 0)+1);
-        }
-    }
-
-    private void resultMathFourth(int matchCount){
-        if(matchCount == LottoStatistics.FOURTH.getMatchCount()){
-            statisticsCounter.put(LottoStatistics.FOURTH, statisticsCounter.getOrDefault(LottoStatistics.FOURTH, 0)+1);
-        }
-    }
-
-    private void resultMathFifth(int matchCount){
-        if(matchCount == LottoStatistics.FIFTH.getMatchCount()){
-            statisticsCounter.put(LottoStatistics.FIFTH, statisticsCounter.getOrDefault(LottoStatistics.FIFTH, 0)+1);
-        }
-    }
-
-    private void resultMathFifthAndBonus(int matchCount, boolean bonusNumber){
-        if(matchCount == LottoStatistics.FIFTH_AND_BONUS.getMatchCount() && bonusNumber == LottoStatistics.FIFTH_AND_BONUS.isMatchBonus()){
-            statisticsCounter.put(LottoStatistics.FIFTH_AND_BONUS, statisticsCounter.getOrDefault(LottoStatistics.FIFTH_AND_BONUS, 0)+1);
-        }
-    }
-
-    private void resultMatchSixth(int matchCount){
-        if(matchCount == LottoStatistics.SIXTH.getMatchCount()){
-            statisticsCounter.put(LottoStatistics.SIXTH, statisticsCounter.getOrDefault(LottoStatistics.SIXTH, 0)+1);
+    private void upMatchCount(int matchCount, boolean isBonusNumberInWinNumber){
+        for (LottoStatistics statistics : LottoStatistics.values()){
+            if (matchCount == statistics.getMatchCount() && statistics.isMatchBonus() == isBonusNumberInWinNumber) {
+                statisticsCounter.put(statistics, statisticsCounter.getOrDefault(statistics, 0)+1);
+                break;
+            }
         }
     }
 
@@ -72,12 +38,4 @@ public class LottoManagerRepository {
         return statisticsCounter;
     }
 
-    public double getLottoRate(int lottoAmount) {
-        double lottoRevenue = 0;
-        for (LottoStatistics statistics : LottoStatistics.values()){
-            lottoRevenue += statistics.getPrice() * statisticsCounter.getOrDefault(statistics, 0);
-        }
-        double rate = (lottoRevenue / lottoAmount) * 100;
-        return Math.round(rate * 100.0) / 100.0;
-    }
 }
