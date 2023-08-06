84e2c97af2b642a131119eb943c0bf6277570fbb
GaBaljaintheroom <junsu1222@naver.com>
2023-08-06 13:08:26 +0900

    refactor: LottoRule 적용

--- src/main/java/lotto/member/exception/InputValidator.java
+++ src/main/java/lotto/member/exception/InputValidator.java
@@ -12,7 +12,7 @@ public class InputValidator {
     }
 
     private static void validateInputThousands(String lottoPrice) {
-        if(Integer.parseInt(lottoPrice) % 1000 != 0){
+        if(Integer.parseInt(lottoPrice) % LottoRule.PRICE_UNIT.getValue() != 0){
             throw new IllegalArgumentException(InputErrorMessage.INPUT_THOUSAND.getMessage());
         }
     }
@@ -26,7 +26,7 @@ public class InputValidator {
 
     public void validateInputLottoNumber(List<Integer> numbers) {
         Set<Integer> numberCount = new HashSet<>(numbers);
-        if (numberCount.size() != 6) {
+        if (numberCount.size() != LottoRule.SIZE.getValue()) {
             throw new IllegalArgumentException(InputErrorMessage.INPUT_NOT_DUPLICATE_AND_SIX.getMessage());
         }
     }
@@ -34,13 +34,13 @@ public class InputValidator {
     public void validateInputBonusNumber(String bonusNumber, List<Integer> winLottoNumber) {
         validateInputNumber(bonusNumber);
         int bonusNumberInt = Integer.parseInt(bonusNumber);
-        if (isNotNumberInRange(bonusNumberInt) && isNumberContainInList(bonusNumberInt, winLottoNumber)){
+        if (isNotNumberInRange(bonusNumberInt) || isNumberContainInList(bonusNumberInt, winLottoNumber)){
             throw new IllegalArgumentException(InputErrorMessage.INPUT_NUMBER_RANGE.getMessage());
         }
     }
 
     private boolean isNotNumberInRange(int number){
-        return number < 1 || number > 45;
+        return number < LottoRule.START_NUMBER.getValue() || number > LottoRule.END_NUMBER.getValue();
     }
 
     private boolean isNumberContainInList(int bonusNumberInt, List<Integer> winLottoNumber){
