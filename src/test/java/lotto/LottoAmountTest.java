298dee6771aa95d8d9cf882493de1a3cf540d0fc
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-07 01:53:49 +0900

    feat(LottoAmountTest): 로또 금액 유효성 검사

--- src/test/java/lotto/LottoAmountTest.java
+++ src/test/java/lotto/LottoAmountTest.java
@@ -2,31 +2,30 @@ package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class LottoAmountTest {
 
+    private final InputValidator inputValidator = new InputValidator();
+
     @DisplayName("1000원 단위가 아닌 금액 입력")
     @Test
     void inputThousandWonUnit() {
-        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount("1001"))
+        assertThatThrownBy(() -> inputValidator.validatePurchaseAmount("1001"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("숫자가 아닌 금액 입력")
     @Test
     void inputNotNumberAmount() {
-        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount("adsf"))
+        assertThatThrownBy(() -> inputValidator.validatePurchaseAmount("adsf"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("공백이 들어간 금액 입력")
     @Test
     void inputAmountWithBlank() {
-        assertThatThrownBy(() -> InputValidator.validatePurchaseAmount("  3000   "))
+        assertThatThrownBy(() -> inputValidator.validatePurchaseAmount("  3000   "))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
