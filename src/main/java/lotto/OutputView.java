2c39efe9b1d84529325b06cf7e079da147322c49
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-07 02:00:53 +0900

    feat(OutputView): 당첨 통계 출력

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -14,8 +14,39 @@ public class OutputView {
         System.out.printf((PURCHASE_QUANTITY), purchaseQuantity);
         return purchaseQuantity;
     }
-    
     public void printLotto(List<Lotto> lottoNumbers) {
         lottoNumbers.forEach(lotto -> System.out.println(lotto.getNumbers()));
+    }
+
+    public void printWinningStatistics(Map<WinningGrade, Integer> winGames, String profitRate) {
+        System.out.println(WINNING_STATISTICS);
+        getWinningStatistics(winGames);
+        getProfitRate(profitRate);
+    }
+
+    private static void getWinningStatistics(Map<WinningGrade, Integer> winGames) {
+        winGames.forEach((grade, count) -> {
+            String reward = getReward(grade, count);
+            System.out.println(reward);
+        });
+    }
+
+    private static String getReward(WinningGrade grade, int count) {
+        if (grade == WinningGrade.SECOND) {
+            return String.format("%d개 일치, 보너스 볼 일치 (%s원) - %d개", grade.getWinningNumberCount(), convertDecimalFormat(
+                    grade.getWinningAmount()), count);
+        }
+        return String.format("%d개 일치 (%s원) - %d개", grade.getWinningNumberCount(), convertDecimalFormat(
+                grade.getWinningAmount()), count);
+    }
+
+    private static String convertDecimalFormat(int cashPrize) {
+        DecimalFormat decimalFormat = new DecimalFormat("###,###");
+        return decimalFormat.format(cashPrize);
+    }
+
+    private static void getProfitRate(String profitRate) {
+        String totalProfitRate = String.format("총 수익률은 %s%%입니다.", profitRate);
+        System.out.println(totalProfitRate);
     }
 }
