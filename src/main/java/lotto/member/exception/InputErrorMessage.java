95a8dbdc5b4a4b8ec23c9597dc1034d38e9d65d2
GaBaljaintheroom <junsu1222@naver.com>
2023-08-06 15:50:46 +0900

    refactor: InputErrorMessage enum 객체 분할

--- src/main/java/lotto/member/exception/InputErrorMessage.java
+++ src/main/java/lotto/member/exception/InputErrorMessage.java
@@ -4,7 +4,8 @@ public enum InputErrorMessage {
     THOUSAND("로또 구입 금액은 1,000 단위 입니다."),
     NUMBER("로또 구입 금액은 오로지 숫자입니다."),
     NOT_DUPLICATE_AND_SIX("로또 번호는 중복되지 않는 6자리 수 입니다."),
-    NUMBER_RANGE("보너스 번호는 1~45 사이의 당첨 번호에 포함되지 않는 숫자입니다.");
+    IN_NUMBER_RANGE("보너스 번호는 1~45 사이의 숫자입니다."),
+    IN_WIN_LOTTO_NUMBER("당첨 번호에 포함되어 있는 숫자입니다.");
 
     private final String message;
     private final String error = "[ERROR] ";
