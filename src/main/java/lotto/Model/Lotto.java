ae13c466f7b94abf060f96728a621e61393dbcbd
김연진 <love_homebody@naver.com>
2023-08-07 03:11:33 +0900

    feat(Lotto): 중복된 숫자 입력시 예외 처리 기능 구현

--- src/main/java/lotto/Model/Lotto.java
+++ src/main/java/lotto/Model/Lotto.java
@@ -1,22 +1,35 @@
 package lotto.Model;
 
+import lotto.view.ExceptionMessage;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int LOTTO_SIZE = 6;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoSize(numbers);
+        validateDuplicateNumber(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            ExceptionMessage message = ExceptionMessage.INCORRECT_LOTTO_SIZE;
+            throw new IllegalArgumentException(message.toString());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> numberToSet = new HashSet<>(numbers);
+        if (numberToSet.size() != LOTTO_SIZE) {
+            ExceptionMessage message = ExceptionMessage.DUPLICATE_NUMBER;
+            throw new IllegalArgumentException(message.toString());
+        }
+    }
 
     public List<Integer> getNumbers() {
         return numbers;
