a5a93e7744d6dc718f3a38e7498c0cab72ef0434
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-07 01:56:40 +0900

    feat(InputView): 당첨 번호, 보너스 번호 입력 뷰

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,25 +1,50 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
-
-import static lotto.message.ProcessMessage.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
-    private static final int LOTTO_PRICE = 1000;
-    InputValidator inputValidator = new InputValidator();
+    private static final String INPUT_PURCHASE_AMOUNT = "구매금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER = "\n당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+    private static final String COMMA = ",";
+
+    private final InputValidator inputValidator = new InputValidator();
+    private List<Integer> winningNumbers;
 
     public int inputPurchaseAmount() {
-        System.out.println(INPUT_AMOUNT_MESSAGE.getMessage());
+        System.out.println(INPUT_PURCHASE_AMOUNT);
         String purchaseAmount = Console.readLine();
         inputValidator.validatePurchaseAmount(purchaseAmount);
-        return Integer.parseInt(purchaseAmount)/LOTTO_PRICE;
+        return Integer.parseInt(purchaseAmount);
     }
 
-    public void inputWinningNumbers() {
-        System.out.println(INPUT_WINNING_MESSAGE.getMessage());
+    public List<Integer> inputWinningNumbers() {
+        System.out.println(INPUT_WINNING_NUMBER);
+        winningNumbers = splitByComma(Console.readLine());
+        inputValidator.validateWinningNumbers(winningNumbers);
+        return winningNumbers;
     }
 
-    public void inputBonusNumber() {
-        System.out.println(INPUT_BONUS_MESSAGE.getMessage());
+    public int inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER);
+        String bonusNumber = Console.readLine();
+        inputValidator.validateBonusNumber(winningNumbers, bonusNumber);
+        return Integer.parseInt(bonusNumber);
+    }
+
+    // 첫 단추를 잘못 끼워서 여기서 에러처리...
+    private List<Integer> splitByComma(String numbers) {
+        try {
+            List<String> numberArray = Arrays.asList(numbers.split(COMMA));
+            return numberArray.stream()
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자만 가능합니다.");
+        }
     }
 }
