8b4a8f2330d7a781c15b76150cd13e25b9851a47
GaBaljaintheroom <junsu1222@naver.com>
2023-08-06 21:29:53 +0900

    style: 계산/결과 메서드 Service로 이동

--- src/main/java/lotto/lottoManager/service/LottoManagerService.java
+++ src/main/java/lotto/lottoManager/service/LottoManagerService.java
@@ -4,6 +4,7 @@ import lotto.lottoManager.repository.LottoManagerRepository;
 import lotto.producer.repository.Lotto;
 
 import java.util.List;
+import java.util.Map;
 
 public class LottoManagerService {
 
@@ -14,19 +15,44 @@ public class LottoManagerService {
     }
 
     public void getStatistics(List<Lotto> lottos, List<Integer> winNumber, int bonusNumber) {
-        // 비교를 한다.
         for (Lotto lotto : lottos) {
             List<Integer> lottoNumbers = lotto.getNumbers();
-            lottoManagerRepository.upMatchCount(lottoNumbers, winNumber, bonusNumber);
+            lottoManagerRepository.upStatisticsMatchCount(lottoNumbers, winNumber, bonusNumber);
         }
-        // 결과를 출력한다.
-        System.out.println();
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        System.out.print(lottoManagerRepository.getMatchResult());
+        System.out.println(getMatchResult());
+
+
+    }
+
+    public String getMatchResult() {
+        Map<LottoStatistics, Integer> statisticsCounter = lottoManagerRepository.getStatisticsCounter();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n")
+                .append("당첨 통계\n")
+                .append("---\n");
+        for (LottoStatistics statistics : LottoStatistics.values()){
+            stringBuilder.append(statistics)
+                    .append(statisticsCounter.getOrDefault(statistics, 0))
+                    .append("개\n");
+        }
+        return stringBuilder.toString();
+
     }
 
     public void getLottoRate(int lottoAmount) {
-        System.out.print("총 수익률은 " + lottoManagerRepository.getLottoRate(lottoAmount) + "%입니다.");
+        String printLottoRate = "총 수익률은 " +
+                calculateLottoRate(lottoAmount) +
+                "%입니다.";
+        System.out.println(printLottoRate);
+    }
+
+    public double calculateLottoRate(int lottoAmount) {
+        Map<LottoStatistics, Integer> statisticsCounter = lottoManagerRepository.getStatisticsCounter();
+        double lottoRevenue = 0;
+        for (LottoStatistics statistics : LottoStatistics.values()){
+            lottoRevenue += statistics.getPrice() * statisticsCounter.getOrDefault(statistics, 0);
+        }
+        double rate = (lottoRevenue / lottoAmount) * 100;
+        return Math.round(rate * 100.0) / 100.0;
     }
 }
