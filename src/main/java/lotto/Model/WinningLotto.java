41b062d2992211bf26502aa94e013c90f9aef639
김연진 <love_homebody@naver.com>
2023-08-07 03:21:40 +0900

    feat(WinningLotto): 당첨 로또 숫자와 보너스 숫자가 겹칠 경우 예외 처리

--- src/main/java/lotto/Model/WinningLotto.java
+++ src/main/java/lotto/Model/WinningLotto.java
@@ -1,13 +1,23 @@
 package lotto.Model;
 
+import lotto.view.ExceptionMessage;
+
 public class WinningLotto {
 
     private final Lotto lotto;
     private final BonusNumber bonusNumber;
 
     public WinningLotto(Lotto lotto, BonusNumber bonusNumber) {
+        validateLottoAndBonusNumber(lotto, bonusNumber);
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateLottoAndBonusNumber(Lotto lotto, BonusNumber bonusNumber) {
+        if (lotto.hasNumber(bonusNumber.getBonus())) {
+            ExceptionMessage message = ExceptionMessage.DUPLICATE_WINNING_NUMBER_AND_BONUS;
+            throw new IllegalArgumentException(message.toString());
+        }
     }
 
     public Lotto getLotto() {
--- src/main/java/lotto/view/ExceptionMessage.java
+++ src/main/java/lotto/view/ExceptionMessage.java
@@ -7,6 +7,7 @@ public enum ExceptionMessage {
     INCORRECT_AMOUNT("[ERROR] 금액은 천 원단위로 입력할 수 있습니다."),
     INCORRECT_LOTTO_SIZE("[ERROR] 6개의 숫자만 입력할 수 있습니다."),
     DUPLICATE_NUMBER("[ERROR] 중복된 숫자는 입력할 수 없습니다."),
+    DUPLICATE_WINNING_NUMBER_AND_BONUS("[ERROR] 당첨 숫자와 보너스 숫자가 중복됩니다."),
     NOT_CONTAIN_COMMA("[ERROR] ','로 숫자를 구분하여 주십시오.");
 
     private final String message;
