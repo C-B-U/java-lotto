f75f5d4239369d17ba710492baefebbd1ed5c4bf
김연진 <love_homebody@naver.com>
2023-08-07 03:17:06 +0900

    feat(Lotto): 범위 이외 숫자가 있는 경우 예외 처리 기능 구현

--- src/main/java/lotto/Model/Lotto.java
+++ src/main/java/lotto/Model/Lotto.java
@@ -9,10 +9,13 @@ import java.util.Set;
 public class Lotto {
     private final List<Integer> numbers;
     private static final int LOTTO_SIZE = 6;
+    private static final int MAX_NUMBER = 45;
+    private static final int MIN_NUMBER = 1;
 
     public Lotto(List<Integer> numbers) {
         validateLottoSize(numbers);
         validateDuplicateNumber(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -28,6 +31,16 @@ public class Lotto {
         if (numberToSet.size() != LOTTO_SIZE) {
             ExceptionMessage message = ExceptionMessage.DUPLICATE_NUMBER;
             throw new IllegalArgumentException(message.toString());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        numbers.forEach(this::isCorrectRange);
+    }
+    private void isCorrectRange(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            ExceptionMessage exceptionMessage = ExceptionMessage.INCORRECT_RANGE;
+            throw new IllegalArgumentException(exceptionMessage.toString());
         }
     }
 
