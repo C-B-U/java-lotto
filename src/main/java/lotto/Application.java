dd7ecb6282ae93326f7fd53142f31341189d8bb8
김승진 <ohksj77@gmail.com>
2023-08-07 12:34:06 +0900

    feat: 에러 핸들링
    기능추가
    - 에러 try catch

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,6 +4,10 @@ public class Application {
     public static void main(String[] args) {
         final ComponentFactory componentFactory = new ComponentFactory();
         final FrontController frontController = componentFactory.frontController();
-        frontController.playLotto();
+        try{
+            frontController.playLotto();
+        } catch(final IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
