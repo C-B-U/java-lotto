ae066767756cbe8ae55f148339fbd521356f7805
Jiyoon Kim <rlawldbs0911@naver.com>
2023-08-07 10:46:40 +0900

    feat(ErrorMessage): 에러 메시지 enum

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -7,7 +7,7 @@ public enum ErrorMessage {
     BONUS_IS_ONLY_NUMERIC("[ERROR] 보너스 번호는 숫자만 입력 가능합니다."),
     PURCHASE_AMOUNT_IS_ONLY_NUMERIC("[ERROR] 구매 금액은 숫자만 입력 가능합니다."),
     PURCHASE_AMOUNT_HAS_NOT_BLANK("[ERROR] 구매 금액은 공백이 없어야 합니다."),
-    LOTTO_HAS_DUPLICATED( "[ERROR] 로또 번호에 중복되는 숫자가 존재합니다."),
+    LOTTO_HAS_DUPLICATED("[ERROR] 로또 번호에 중복되는 숫자가 존재합니다."),
     LOTTO_HAS_SIX_NUMBER("[ERROR] 로또 번호는 6개로 구성되어야 합니다."),
     LOTTO_IS_1_MORE_OR_45_LESS("[ERROR] 로또 번호는 1이상 45이하여야 합니다."),
     BONUS_IS_1_MORE_OR_45_LESS("[ERROR] 보너스 번호는 1이상 45이하여야 합니다.");
