40730202d0a23f474cdfdbf9ec61098f8c4d891b
김연진 <love_homebody@naver.com>
2023-08-09 00:10:50 +0900

    refactor: 당첨 금액 언더바 추가

--- src/main/java/lotto/Model/Ranking.java
+++ src/main/java/lotto/Model/Ranking.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public enum Ranking {
     NONE(0, 0, false, null),
-    FIFTH(5000, 3, false, "3개 일치 (5,000원) - %d개"),
-    FOURTH(50000, 4, false, "4개 일치 (50,000원) - %d개"),
-    THIRD(1500000, 5, false, "5개 일치 (1,500,000원) - %d개"),
-    SECOND(30000000, 5, true, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    FIRST(2000000000, 6, false, "6개 일치 (2,000,000,000원) - %d개");
+    FIFTH(5_000, 3, false, "3개 일치 (5,000원) - %d개"),
+    FOURTH(50_000, 4, false, "4개 일치 (50,000원) - %d개"),
+    THIRD(1_500_000, 5, false, "5개 일치 (1,500,000원) - %d개"),
+    SECOND(30_000_000, 5, true, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    FIRST(2_000_000_000, 6, false, "6개 일치 (2,000,000,000원) - %d개");
 
     private final int prize;
     private final int matchNumber;
