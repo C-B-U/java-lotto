e58053193e135491234f15644585cb04fddd1dee
yeonjy <yeonjypaper@gmail.com>
2023-08-29 05:00:15 +0900

    refactor: ", " -> CRITERIA

--- src/main/java/lotto/InputProcessor.java
+++ src/main/java/lotto/InputProcessor.java
@@ -3,9 +3,9 @@ package lotto;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class InputProcessor {
+    private static final String CRITERIA = ", ";
 
     public int changeToInt(String money) {
         try {
@@ -16,7 +16,7 @@ public class InputProcessor {
     }
 
     public List<Integer> changeToIntegerList(String s) {
-        return Arrays.stream(s.split(", "))
+        return Arrays.stream(s.split(CRITERIA))
                 .map(Integer::parseInt).collect(Collectors.toList());
     }
 }
