31151e5311f1b91d2c1963d4a764616f690d5414
yeonjy <yeonjypaper@gmail.com>
2023-08-28 17:48:08 +0900

    refactor: 상수 -> enum

---
+++ src/main/java/lotto/LottoRangeNumber.java
@@ -0,0 +1,20 @@
+package lotto;
+
+public enum LottoRangeNumber {
+    MIN_LOTTO_NUM(1),
+    MAX_LOTTO_NUM(45),
+    NUM_OF_LOTTO_NUMS(6)
+    ;
+
+
+    private int num;
+
+    LottoRangeNumber(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+}
--- src/main/java/lotto/repositroy/Lotto.java
+++ src/main/java/lotto/repositroy/Lotto.java
@@ -1,4 +1,7 @@
-package lotto;
+package lotto.repositroy;
+
+import lotto.ErrorMessage;
+import lotto.LottoRangeNumber;
 
 import java.util.List;
 
@@ -17,20 +20,21 @@ public class Lotto {
     }
 
     private void checkSixNumbers(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException(ErrorMessage.NOT_SIX_NUMBERS.toString());
+        if (numbers.size() != LottoRangeNumber.NUM_OF_LOTTO_NUMS.getNum()) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_SIX_NUMBERS.getMessage());
         }
     }
 
      private void checkNotDuplicated(List<Integer> numbers) {
          if (numbers.size() != numbers.stream().distinct().count()) {
-             throw new IllegalArgumentException(ErrorMessage.DUPLICATED_NUMBERS.toString());
+             throw new IllegalArgumentException(ErrorMessage.DUPLICATED_NUMBERS.getMessage());
          }
      }
 
     private void checkInRange(List<Integer> numbers) {
-        if (!numbers.stream().allMatch(n -> n <= 45 && n >= 1)) {
-            throw new IllegalArgumentException(ErrorMessage.OUT_OF_RANGE_NUMBER.toString());
+        if (!numbers.stream().allMatch(n -> n <= LottoRangeNumber.MAX_LOTTO_NUM.getNum()
+                && n >= LottoRangeNumber.MIN_LOTTO_NUM.getNum())) {
+            throw new IllegalArgumentException(ErrorMessage.OUT_OF_RANGE_NUMBER.getMessage());
         }
     }
 }
