231bb0ed04a06d56b3145c1f5ced75d4aa1d11ff
yeonjy <yeonjypaper@gmail.com>
2023-08-29 00:12:33 +0900

    refactor: Integer -> int

--- src/main/java/lotto/repositroy/BonusLotto.java
+++ src/main/java/lotto/repositroy/BonusLotto.java
@@ -6,25 +6,25 @@ import lotto.LottoRangeNumber;
 import java.util.List;
 
 public class BonusLotto {
-    private final Integer number;
+    private final int number;
 
-    public BonusLotto(Integer number, List<Integer> winNumbers) {
+    public BonusLotto(int number, List<Integer> winNumbers) {
         validate(number, winNumbers);
         this.number = number;
     }
 
-    private void validate(Integer number, List<Integer> winNumbers) {
+    private void validate(int number, List<Integer> winNumbers) {
         checkInRage(number);
         checkNotDuplicated(number, winNumbers);
     }
 
-    private void checkInRage(Integer number) {
+    private void checkInRage(int number) {
         if (number > LottoRangeNumber.MAX_LOTTO_NUM.getNum() || number < LottoRangeNumber.MIN_LOTTO_NUM.getNum()) {
             throw new IllegalArgumentException(ErrorMessage.OUT_OF_RANGE_NUMBER.getMessage());
         }
     }
 
-    private void checkNotDuplicated(Integer number, List<Integer> winNumbers) {
+    private void checkNotDuplicated(int number, List<Integer> winNumbers) {
         if (winNumbers.contains(number)) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATED_NUMBERS.getMessage());
         }
