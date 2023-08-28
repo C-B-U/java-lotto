372a03dd1c68963316efecdda6c417d8d56aee35
yeonjy <yeonjypaper@gmail.com>
2023-08-29 05:11:46 +0900

    feat: 로또 번호 일치 개수 enum

---
+++ src/main/java/lotto/service/MatchNum.java
@@ -0,0 +1,30 @@
+package lotto.service;
+
+
+public enum MatchNum {
+    THREE_MATCH(3),
+    FOUR_MATCH(4),
+    FIVE_MATCH(5),
+    BONUS_MATCH(7),
+    SIX_MATCH(6);
+
+    private final int num;
+
+    MatchNum(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public static MatchNum valueOf(int check) {
+        for (MatchNum matchNum : MatchNum.values()) {
+            if (check == matchNum.getNum()) {
+                return matchNum;
+            }
+        }
+        return null;
+    }
+
+}
