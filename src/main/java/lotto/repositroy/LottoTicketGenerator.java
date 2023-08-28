e64b7ccea1e53016fc3dcda5a3905006497a153a
yeonjy <yeonjypaper@gmail.com>
2023-08-28 22:29:30 +0900

    feat: 로또 티켓 생성 구현

---
+++ src/main/java/lotto/repositroy/LottoTicketGenerator.java
@@ -0,0 +1,31 @@
+package lotto.repositroy;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.LottoRangeNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoTicketGenerator {
+    private final List<List<Integer>> tickets;
+
+    public LottoTicketGenerator() {
+        tickets = new ArrayList<>();
+    }
+
+    public void generateTicket(int ticketNum) {
+        for (int i = 0; i < ticketNum; i++) {
+            tickets.add(generateNumber());
+        }
+    }
+
+    private List<Integer> generateNumber() {
+        return Randoms.pickUniqueNumbersInRange(LottoRangeNumber.MIN_LOTTO_NUM.getNum()
+                , LottoRangeNumber.MAX_LOTTO_NUM.getNum(), LottoRangeNumber.NUM_OF_LOTTO_NUMS.getNum());
+    }
+
+    public List<List<Integer>> getSortedTickets() {
+        return tickets;
+    }
+
+}
