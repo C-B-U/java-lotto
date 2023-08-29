17509112f31e49929a855388c7a25f92e8cf6833
yeonjy <yeonjypaper@gmail.com>
2023-08-29 14:10:44 +0900

    feat: 로또 실행 결과 출력 가이드 enum

--- src/main/java/lotto/service/OutputGuide.java
+++ src/main/java/lotto/service/OutputGuide.java
@@ -2,7 +2,11 @@ package lotto.service;
 
 public enum OutputGuide {
     NUMBER_OF_PURCHASE("개를 구매했습니다."),
-    RESULT_STATISTICS("당첨 통계\n---");
+    RESULT_STATISTICS("당첨 통계\n---"),
+
+    EQUAL_NUM_GUIDE("개 일치 ("),
+    PRIZE_MONEY_GUIDE("원) - "),
+    LOTTO_NUM("개");
 
     private final String content;
 
