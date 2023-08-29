2fea1497b9fc883f99c31702c9c7bd7606f097eb
yeonjy <yeonjypaper@gmail.com>
2023-08-29 15:46:54 +0900

    feat: 수익률 출력 가이드 enum

--- src/main/java/lotto/service/OutputGuide.java
+++ src/main/java/lotto/service/OutputGuide.java
@@ -5,8 +5,12 @@ public enum OutputGuide {
     RESULT_STATISTICS("당첨 통계\n---"),
 
     EQUAL_NUM_GUIDE("개 일치 ("),
+    BONUS_NUM_GUIDE("5개 일치, 보너스 볼 일치 ("),
     PRIZE_MONEY_GUIDE("원) - "),
-    LOTTO_NUM("개");
+    LOTTO_NUM("개"),
+
+    TOTAL_EARNING_FRONT("총 수익률은 "),
+    TOTAL_EARNING_BACK("%입니다.");
 
     private final String content;
 
